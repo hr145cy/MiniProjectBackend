e48522b994390ee4bdf716bc3ab69b31dfafdbce
Your Name <youremail@example.com>
2026-03-02 22:00:36 +0530

    made the profile route (which when opens only user logs in)

--- app.js
+++ app.js
@@ -19,11 +19,24 @@ app.get('/login', (req, res) => {
     res.render("login");
 });
 
-app.get('/profile', isLoggedIn, (req, res) => {
-    console.log(req.user);
-    res.render("login");
+app.get('/profile', isLoggedIn, async (req, res) => {
+    let user= await userModel.findOne({email: req.user.email}).populate("posts");
+    res.render("profile", {user});
 });
 
+app.post('/post', isLoggedIn, async (req, res) => {
+    let user= await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+});
 
 app.post('/register', async (req, res) => {
     let {email, password, username, name, age} = req.body;
@@ -43,7 +56,7 @@ app.post('/register', async (req, res) => {
 
             let token= jwt.sign({email: email, userid: user._id}, "shhhh");
             res.cookie("token", token);
-            res.send("registered");
+            res.send("/profile");
         })
     })
 });
@@ -56,9 +69,9 @@ app.post('/login', async (req, res) => {
 
     bcrypt.compare(password, user.password, function(err, result){
         if(result){
-            res.status(200).send("you can login");
             let token= jwt.sign({email: email, userid: user._id}, "shhhh");
             res.cookie("token", token);
+            res.status(200).redirect("/profile");
         }
         else res.redirect("/login");
     })
@@ -70,13 +83,12 @@ app.get('/logout', (req, res) => {
 });
 
 function isLoggedIn(req, res, next){
-    if(req.cookies.token === "") res.send("You must be logged in");
+    if(req.cookies.token == "") res.redirect("/login");
     else{
         let data = jwt.verify(req.cookies.token, "shhhh");
         req.user = data;
         next();
     }
-    
 }
 
 app.listen(3000);
